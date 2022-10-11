1bfcbb8519d8e48ae01ac1874d4098625f971207
= <=>
2022-10-11 16:37:58 +0400

    added task load after mark and corrected css styles

--- client/src/components/Pages/Home.vue
+++ client/src/components/Pages/Home.vue
@@ -75,7 +75,7 @@
                             <button type="button" v-bind:class="data.status_id == 4 ? 'btn btn-success' : 'btn btn-secondary' " @click="markAs($event)" ref="mark" :data-task-id="data.task_id"><BIconCheckCircleFill class="pointer" /></button>
                             <span>&nbsp;&nbsp;&nbsp;{{ data.title }}</span>
                         </td>
-                        <td>
+                        <td class="desc">
                             <span v-html="data.description"></span>
                         </td>
                         <td>
@@ -146,6 +146,36 @@
 <style lang="scss" scoped>
     .card {
         margin-left: 50px !important;
+        border-radius: 10px;
+    }
+
+    .bg-info {
+        background-color: lighten(#0dcaf0, 35%) !important;
+        color: #0dcaf0 !important;
+        border: none;
+    }
+
+    .bg-warning {
+        background-color: lighten(#ffc107, 35%) !important;
+        color: #ffc107 !important;
+        border: none;
+    }
+
+    .bg-danger {
+        background-color: lighten(#dc3545, 35%) !important;
+        color: #dc3545 !important;
+        border: none;
+    }
+
+    .bg-success {
+        background-color: lighten(#198754, 35%) !important;
+        color: #198754 !important;
+        border: none;
+    }
+
+    .card-header {
+        border: none;
+        background-color: rgba(#fff, .0);
     }
 
     table {
@@ -154,5 +184,11 @@
 
     .pointer {
         pointer-events: none;
+    }
+
+    .desc {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
     }
 </style>
