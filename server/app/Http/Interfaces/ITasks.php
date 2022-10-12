357fc1fa0d5ea4747e7c6559331ab92555cb0275
= <=>
2022-10-12 17:12:31 +0400

    fixed bugs

--- server/app/Http/Interfaces/ITasks.php
+++ server/app/Http/Interfaces/ITasks.php
@@ -6,16 +6,16 @@
 
     // ინტერფეისი თასკების კლასისთვის
     interface ITasks {
-        public function Add_Task(AddTaskRequest $request); // თასქის დამატების მეთოდი
+        public function Add_Task(AddTaskRequest $request); // დავალების დამატების მეთოდი
 
-        public function Task_Delete(int $id); // თასქის წაშლის მეთოდი
+        public function Task_Delete(int $id); // დავალების წაშლის მეთოდი
 
-        public function Edit_Task(EditTaskRequest $request, int $id); // თასქის რედაქტირების მეთოდი
+        public function Edit_Task(EditTaskRequest $request, int $id); // დავალების რედაქტირების მეთოდი
 
         public function Task_List(); // დავალებების წამოღების მეთოდი
 
-        public function Mark_Task_As_Done(int $id); //თასქის შესრულებულად მონიშვნის მეთოდი
+        public function Mark_Task_As_Done(int $id); //დავალების შესრულებულად მონიშვნის მეთოდი
 
-        public function Get_Task(int $id); // თასქის წამოღების მეთოდი
+        public function Get_Task(int $id); // დავალების წამოღების მეთოდი
     }
 ?>
