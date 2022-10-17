5a29e3d2b987fc234e1b6c004cbf6aa0cd9f3331
= <=>
2022-10-17 10:53:32 +0400

    added softdeletes to the files

--- server/app/Models/TaskFile.php
+++ server/app/Models/TaskFile.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Task;
 
 class TaskFile extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = "files";
 
@@ -16,6 +17,10 @@ class TaskFile extends Model
 
     protected $fillable = [
         "file", "task_id"
+    ];
+
+    protected $dates = [
+        "deleted_at"
     ];
 
     public $timestamps = true;
